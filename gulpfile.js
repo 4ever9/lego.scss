7c81653680a3ebfec1e5d7e7bbf8ed8f46b3d5ba
Aiden X <caichao.xu@gmail.com>
2020-07-08 19:41:04 +0800

    feat(grid): add gen grid mixin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 const {src, dest, watch, parallel, series} = require('gulp');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
 const connect = require('gulp-connect');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
-const concat = require('gulp-concat');
 sass.compiler = require('node-sass');
 
 function serve() {
@@ -17,18 +12,19 @@ function serve() {
 }
 
 function compileMox() {
-    return src('mox.scss')
+    return src('./*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        // .pipe(sass().on('error', sass.logError))
         .pipe(dest('./dist'))
         .pipe(connect.reload());
 }
 
 exports.default = series(parallel(compileMox), function () {
-    watch('mox.scss', compileMox)
+    watch('./*.scss', compileMox)
 })
 
 exports.dev = series(parallel(compileMox), parallel(serve, function () {
-    watch('mox.scss', compileMox)
+    watch('./*.scss', compileMox)
     watch('./*.html', function () {
         return src('./*.html')
             .pipe(dest('./'))
