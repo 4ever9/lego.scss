185823b37c7a7649c4d178b045ffaaf7adedeb3a
Aiden X <caichao.xu@gmail.com>
2020-10-10 00:53:00 +0800

    form: add pure type

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,46 @@
-const {src, dest, watch, parallel, series} = require('gulp');
-const connect = require('gulp-connect');
-const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
+const { src, dest, watch, parallel, series } = require("gulp");
+const connect = require("gulp-connect");
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
 
 const resources = {
-    'scss': './*.scss',
-    'html': './*.html',
-    'dest': './dist',
-}
+  scss: "./*.scss",
+  html: "./*.html",
+  dest: "./dist",
+};
+
+const includePaths = ["node_modules/layout.scss/src/scss"];
 
 function serve() {
-    return connect.server({
-        root: '.',
-        livereload: true,
-        port: 3030
-    });
+  return connect.server({
+    root: ".",
+    livereload: true,
+    port: 3030,
+  });
 }
 
 function compileScss() {
-    return src('demo.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(dest(resources.dest))
-        .pipe(connect.reload());
+  return src("demo.scss")
+    .pipe(
+      sass({
+        includePaths: includePaths,
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(dest(resources.dest))
+    .pipe(connect.reload());
 }
 
 exports.default = series(compileScss, function () {
-    watch(resources.scss, compileScss)
-})
+  watch(resources.scss, compileScss);
+});
 
-exports.dev = series(parallel(compileScss), parallel(serve, function () {
-    watch(resources.scss, compileScss)
+exports.dev = series(
+  parallel(compileScss),
+  parallel(serve, function () {
+    watch(resources.scss, compileScss);
     watch(resources.html, function () {
-        return src(resources.html)
-            .pipe(connect.reload());
-    })
-}))
+      return src(resources.html).pipe(connect.reload());
+    });
+  })
+);
